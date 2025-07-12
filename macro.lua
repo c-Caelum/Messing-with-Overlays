c98e2563f09973ce95992806f8048765e733b1ea
Caelum <136895359+tobi-glitch@users.noreply.github.com>
2025-07-13 00:47:54 +0800

    Create macro.lua

---
+++ macro.lua
@@ -0,0 +1,40 @@
+Dioscuri Gambit
+Dioscuri Gambit
+Archer's Distillation
+Gemini Decomposition
+Nullary Reflection
+Equality Distillation
+Introspection
+    Bookkeeper's Gambit: vvvv-
+Retrospection
+Introspection
+    Rotation Gambit II
+    Architect's Distillation
+    Undertaker's Gambit
+    Numerical Reflection: .5
+    Multiplicative Distillation
+    Additive Distillation
+    Prospector's Gambit
+    Numerical Reflection: -1
+    Multiplicative Distillation
+    Multiplicative Distillation
+    Numerical Reflection: 3
+    Fisherman's Gambit II
+    Numerical Reflection: 2
+    Fisherman's Gambit II
+    Multiplicative Distillation
+    Additive Distillation
+    Numerical Reflection: -1
+    Multiplicative Distillation
+    Rotation Gambit II
+    Prospector's Gambit
+    Jester's Gambit
+    Multiplicative Distillation
+    Jester's Gambit
+    Rotation Gambit II
+    Division Distillation
+    Multiplicative Distillation
+    Additive Distillation
+Retrospection
+Augur's Exaltation
+Hermes' Gambit
